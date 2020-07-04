2d54b784c0c64417cc48e7941f9636bb05f64219
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-07-03 22:14:40 -0300

    Frontend final com ajustes.

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -8,13 +8,13 @@ const debtsApi = new DebtsApi()
 class Cadastro extends Component{
   constructor(props) {
     super(props)
-  }
+  };
   
   state = {users: [{}], debts: []}
   
   componentDidMount = async() => {
     this.refreshData()
-  }
+  };
   
   async refreshData() {
     const debts = debtsApi.listDebts()
@@ -25,11 +25,22 @@ class Cadastro extends Component{
       debts: await debts,
       users: await users
     })
-  }
+  };
 
   submitForm = async function (params) {
-  
+    
+    if (!this.refs.user.value || !this.refs.debtName.value || !this.refs.value.value || ! this.refs.date.value) {
+      this.setState({message: 'Todos os campos são obrigatórios.'})
+      return;
+    }
+
+    if (this.refs.value.value <= 0) {
+      this.setState({message: 'O valor da dívida precisa ser maior que 0.'});
+      return;
+    }
+
     const payload = {
+      "id": this.refs.id.value,
       "user": {
         "id": this.refs.user.value,
         "name": this.refs.user.selectedOptions[0].innerText
@@ -37,41 +48,74 @@ class Cadastro extends Component{
       "debtName": this.refs.debtName.value,
       "value": this.refs.value.value,
       "date": this.refs.date.value
-      }
-
-    await debtsApi.create(payload);
+    };
+    
+    if (!payload.id) {    
+      await debtsApi.create(payload);
+    } else {
+      await debtsApi.update(payload);
+    }
+    this.state.message = 'A dívida foi salva com sucesso! 😁️';
     this.refreshData();
+    this.cleanForm();
+    } 
+  
+    
+    
+  loadDebt(debt) {
+    this.refs.id.value = debt.id
+    this.refs.user.value = debt.user.id
+    this.refs.debtName.value = debt.debtName
+    this.refs.value.value = debt.value
+    this.refs.date.value = debt.date
+  }
+    
+  listDebts() {
+    return this.state.debts.filter(debt => debt != null).map(debt =>
+      <div className="listDebtsResults" onClick={()=>this.loadDebt(debt)}>
+      {debt.user.name} R$:{debt.value}
+    </div>);
   }
 
-  loadDebt(debitId) {
-    console.log(debitId)     
+  listUserDebts() {
+    return this.state.debts.filter(
+      debt => debt != null && debt.user.id == this.refs.user.value).map(
+      debt => <div className="listDebtsResults" onClick={()=>this.loadDebt(debt)}>
+      {debt.user.name} R$:{debt.value}
+    </div>);
   }
   
-  listDebts() {
-    return this.state.debts.map(debt =>
-      <div className="listDebtsResults" onClick={()=>this.loadDebt(debt.id)}>
-        {debt.user.name} R$:{debt.value}
-      </div>);
+  deleteDebt = async function (params) {    
+    const payload = {"id": this.refs.id.value}
+    await debtsApi.delete(payload);
+    this.state.message = 'A dívida foi apagada com sucesso!';
+    this.refreshData();
+    this.cleanForm();
   }
 
   cleanForm() {
-    // Escrever uma funcao para limpar ou inserir coisas vazias no formulario
+    this.refs.id.value = null
+    this.refs.user.value = null
+    this.refs.debtName.value = null
+    this.refs.value.value = null
+    this.refs.date.value = null
   }
   
   render() {
     return (
       <div className="grid-container">          
           <div className="Content">          
-            <div className="Historic">
-              <h2 className="historicTitle">Histórico</h2><br/>{this.listDebts()}
+            <h2 className="TitleHistoric">Histórico</h2>
+              <div className="Historic">{this.listDebts()}
             </div>              
-              <div class="Register">                
-                <h2 className="registerTitle">Cadastro de dívida</h2><br/>
+              <h2 className="TitleRegister">Cadastro de dívida</h2><br/>
+                <div className="Register">                
                 <form >
+                    <input ref="id" type="hidden" name="id"/>
                   
                   Pessoas
                   <div>
-                    <select ref="user" name="user">
+                    <select ref="user" name="user" onChange={()=>this.setState({})} >
                       {this.state.users.map(user => <option value={user.id}>{user.name}</option>)}
                     </select>
                   </div>
@@ -103,16 +147,19 @@ class Cadastro extends Component{
                 </form>
                 <br/>                              
                 <div className="buttons">
-                  <input className="eraseButton"onClick={()=>this.deleteForm()} type="submit" value="Excluir"/>
+                  <input className="eraseButton"onClick={()=>this.deleteDebt()} type="submit" value="Excluir"/>
                   <input className="saveButton"onClick={()=>this.submitForm()} type="submit" value="Salvar"/>                                    
                 </div>   
-                <button className="newDebit" onClick="">Cadastrar nova dívida</button>
+                <button className="newDebit" onClick={()=>this.cleanForm()}>Cadastrar nova dívida</button>
               </div>
+                <h2 className="TitleUserHistoric">Histórico de usuário</h2>
+                <div className="UserHistoric">{this.listUserDebts()}</div>
             </div>
-        <div class="Header">
+        <div className="Header">
           <h1 className="debtManager">Debt Manager</h1>
         </div>
-        <div class="Footer">
+        <div className="Footer">
+          <span className="operationMessage">{this.state.message}</span>
         </div>
         
       </div>      
--- src/service/DebtsApi.js
+++ src/service/DebtsApi.js
@@ -7,14 +7,34 @@ class DebtsApi {
       headers: {'Content-Type': 'application/json'
     },
       body: JSON.stringify(payload)}    
-  )
+  );
     return request;
-  }
+  };
   
   listDebts = async () => {
     const request = await fetch(`${this.host}/debts`);
     return await request.json();
-  }
+  };
+
+  update = (payload) => {
+    const request = fetch(`${this.host}/debts/${payload.id}`, {
+      method:'POST',
+      headers: {'Content-Type': 'application/json'
+    },
+      body: JSON.stringify(payload)}    
+  );
+    return request;
+  };
+
+  delete = (payload) => {
+    const request = fetch(`${this.host}/debts/${payload.id}`, {
+      method:'DELETE',
+      headers: {'Content-Type': 'application/json'
+    },
+      body: JSON.stringify(payload)}    
+  );
+    return request;
+  };
 };
   
 export default DebtsApi;
